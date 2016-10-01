5f6e948211beb81bea738cca383522f6c1144eed
GalvinGao <2403901511@qq.com>
2016-10-02 00:34:01 +0800

    Good night

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     if (password.length < 6) { 
 		$('#result').removeClass()
 		$('#result').addClass('short')
-		return '密码太短了！' 
+		return '密码太短了！'
 	}
     
     //length is ok, lets continue.
@@ -25,7 +25,7 @@ $(document).ready(function() {
 	if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1
 	
 	//if it has numbers and characters, increase strength value
-	if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1 
+	if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1
 	
 	//if it has one special character, increase strength value
     if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1
@@ -38,24 +38,24 @@ $(document).ready(function() {
 	//if value is less than 2
 	if (strength == 0 ) {
 		$('#result').removeClass()
-		$('#result').addClass('weak')
-		return '密码'	
+		$('#result').addClass('r0')
+		return '0'	
     } else if (strength == 1 ) {
 		$('#result').removeClass()
-		$('#result').addClass('good')
-		return 'Good'	
+		$('#result').addClass('r1')
+		return '1'	
 	} else if (strength == 2 ) {
 		$('#result').removeClass()
-		$('#result').addClass('good')
-		return 'Good'	
+		$('#result').addClass('r2')
+		return '2'	
     } else if (strength == 3 ) {
 		$('#result').removeClass()
-		$('#result').addClass('good2')
-		return 'Good2'
+		$('#result').addClass('r3')
+		return '3'
 	} else {
 		$('#result').removeClass()
-		$('#result').addClass('strong')
-		return 'Strong'
+		$('#result').addClass('r4')
+		return '4'
 	}
 }
 });
