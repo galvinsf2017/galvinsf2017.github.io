d2625b60ed037c161ac61ce83fb3d051f1cbc8ca
GalvinGao <2403901511@qq.com>
2017-02-01 15:57:47 +0800

    123

--- js/header.js
+++ js/header.js
@@ -3,3 +3,4 @@
 document.write("<link href=\"css/style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\" crossorigin=\"anonymous\"/>");
 document.write("<link href=\"//cdn.bootcss.com/materialize/0.97.7/css/materialize.min.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\" crossorigin=\"anonymous\"/>")
 document.write("<link href=\"//cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.min.css\" rel=\"stylesheet\" crossorigin=\"anonymous\">");
+document.write("<link href=\"http://ok1a94asp.bkt.clouddn.com/sf2017/fonts/FZQingKeBenYueSongS-R-GB.css\" rel=\"stylesheet\" crossorigin=\"anonymous\">");
