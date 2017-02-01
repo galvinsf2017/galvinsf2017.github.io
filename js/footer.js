ed9edfed987af31acbd1b1bc4403d68b3a4c0956
GalvinGao <2403901511@qq.com>
2017-02-01 15:29:26 +0800

    666

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,12 @@
-document.write("<script src=\"//cdn.bootcss.com/jquery/2.1.1/jquery.min.js\"><\/script>");
-document.write("<script src=\"//cdn.bootcss.com/materialize/0.97.7/js/materialize.min.js\"><\/script>");
-document.write("<script src=\"js/init.min.js\"><\/script>");
-document.write("<link href=\"//cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.min.css\" rel=\"stylesheet\">");
-document.write("<script src=\"//ok1a94asp.bkt.clouddn.com/sf2017/random-quote.min.js\"><\/script>");
-document.write("<script src=\"//ok1a94asp.bkt.clouddn.com/sf2017/hsimp.min.js\"><\/script>");
+// [JavaScript] JS 引用写入
+
+document.write("<script src=\"//cdn.bootcss.com/jquery/2.1.1/jquery.min.js\" crossorigin=\"anonymous\"><\/script>");
+document.write("<script src=\"//cdn.bootcss.com/materialize/0.97.7/js/materialize.min.js\" crossorigin=\"anonymous\"><\/script>");
+document.write("<script src=\"js/init.min.js\" crossorigin=\"anonymous\"><\/script>");
+document.write("<script src=\"//ok1a94asp.bkt.clouddn.com/sf2017/random-quote.min.js\" crossorigin=\"anonymous\"><\/script>");
+document.write("<script src=\"//ok1a94asp.bkt.clouddn.com/sf2017/hsimp.min.js\" crossorigin=\"anonymous\"><\/script>");
+
+
+// [HTML] 版权声明 写入
+
+document.write("<footer class=\"page-footer orange\"><div class=\"container\"><div class=\"row\"><div class=\"col l6 s12\"><h5 class=\"white-text\">高逸扬 Galvin.G<\/h5><p class=\"grey-text text-lighten-4\">在编程方面小有成就，希望大家多多指教~<br>哦对，记得投我一票哦！<br>我的展台号码是：<span class=\"red-text text-lighten-1 number\">NUMBER<\/span><\/p><div class=\"card-panel orange darken-2\"><span class=\"white-text\"><span style=\"font-size:120%\">版权声明<\/span><br><br><a rel=\"license\" href=\"http://creativecommons.org/licenses/by/4.0/\"><img alt=\"知识共享许可协议\" style=\"border-width:0\" src=\"http://ok1a94asp.bkt.clouddn.com/sf2017/80x15.png\"><\/a><span xmlns:dct=\"http://purl.org/dc/terms/\" property=\"dct:title\">&nbsp;&nbsp;&nbsp;| 密码复杂度检测器 |<\/span> 由 <a xmlns:cc=\"http://creativecommons.org/ns#\" href=\"http://galvingao.xyz/\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Galvin.G<\/a> 基于 <a xmlns:dct=\"http://purl.org/dc/terms/\" href=\"https://github.com/howsecureismypassword/hsimp\" rel=\"dct:source\">howsecureismypassword<\/a> 与 <a xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://materializecss.com\" rel=\"dct:source\">Materialize<\/a> 制作，并采用 <a rel=\"license\" href=\"http://creativecommons.org/licenses/by/4.0/\">知识共享 署名 4.0 国际许可协议<\/a> 进行许可。<\/span><\/div><\/div><\/div><\/div><\/footer>");
--- js/header.js
+++ js/header.js
@@ -1,2 +1,5 @@
-document.write("<link href=\"css/style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/>");
-document.write("<link href=\"//cdn.bootcss.com/materialize/0.97.7/css/materialize.min.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/>")
+// [CSS] CSS 引用写入
+
+document.write("<link href=\"css/style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\" crossorigin=\"anonymous\"/>");
+document.write("<link href=\"//cdn.bootcss.com/materialize/0.97.7/css/materialize.min.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\" crossorigin=\"anonymous\"/>")
+document.write("<link href=\"//cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.min.css\" rel=\"stylesheet\" crossorigin=\"anonymous\">");
